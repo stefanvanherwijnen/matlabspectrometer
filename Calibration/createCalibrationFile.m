cc6859059dc6a6f266b95b49fdd1cdb9acb6b2d6
stefan <s.a.h.v.herwijnen@student.tue.nl>
2018-03-12 18:48:42 +0100

    Added scripts for creating a calibration file and for calculating the absolute irradiance of a spectrum using the calibration file.

---
+++ Calibration/createCalibrationFile.m
@@ -0,0 +1,70 @@
+function [ calibrationData ] = createCalibrationFile(fiberdiameter, lampFile, spectrumFile, darkSpectrumFile)
+%CREATECALIBRATIONFILE Calculates calibration data from a raw spectrum and lamp file
+% The calibration file in uJoule/count is calculated from the lamp file
+% [uW/nm/cm^2], the integration time [s], the collection area [cm^2] and
+% the wavelength spread [nm/pixel].
+
+%% Initialization
+lampDirectory = '';
+spectrumDirectory = '';
+darkSpectrumDirectory = '';
+
+%% Set area
+if (~exist('fiberdiameter', 'var'))
+    fiberdiameter = inputdlg('Enter fiber diameter [cm]', 'Fiber diameter' , 1, {'0'});
+    fiberdiameter = str2num(fiberdiameter{1});
+end
+
+%% Select lamp file
+if (~exist('lampFile', 'var'))
+    [lampFile lampDirectory] = uigetfile({'*.lmp';'*.*'}, 'Select the lamp file');
+end
+
+%% Select spectra
+% Load one smoothed spectrum
+if (~exist('spectrumFile', 'var'))
+    [spectrumFile spectrumDirectory] = uigetfile({'*.txt';'*.*'}, 'Select the smoothed spectrum');
+end
+spect = importdata(strcat(spectrumDirectory, spectrumFile));
+Sp = spect.data;
+% Integration time
+IndexC = strfind(spect.textdata, 'Integration Time');
+Index = find(not(cellfun('isempty', IndexC)));
+T = textscan(spect.textdata{9}, 'Integration Time (usec): %f'); % usec
+T = T{1}/1e6;   % sec
+
+% Load dark spectrum
+if (~exist('darkSpectrumFile', 'var'))
+    [darkSpectrumFile darkSpectrumDirectory] = uigetfile({'*.txt';'*.*'}, 'Select the dark spectrum');
+end
+darkSpect = importdata(strcat(darkSpectrumDirectory, darkSpectrumFile));
+Dp = darkSpect.data;
+
+%% Import lamp file and create a spline interpolant fit
+lmp = importdata(strcat(lampDirectory,lampFile));             % µW/cm^2/nm
+fit = spline(lmp(:,1), lmp(:,2), lmp(1,1):0.01:lmp(end,1));
+
+%% Show fit for verification
+figure; hold all;
+plot(lmp(:,1), lmp(:,2), '*');
+plot(lmp(1,1):0.01:lmp(end,1), fit);
+legend('Lamp file', 'Spline interpolant');
+xlabel('Wavelength [nm]', 'Interpreter', 'latex');
+ylabel('Absolute irradiance [$\mathrm{\mu W/cm^2/nm}$]', 'Interpreter', 'latex');
+
+%% Determine joules per count
+A = pi*(fiberdiameter/2)^2;     % cm^2
+dLp = Sp(2,1) - Sp(1,1);        % nm
+lampIrradiance = spline(lmp(:,1), lmp(:,2), Sp(:,1));   % uW/nm/cm^2
+lampIrradiance(Sp(:,1) < lmp(1,1)) = 0; lampIrradiance(Sp(:,1) > lmp(end,1)) = 0;   % Set values outside range to 0
+
+calibrationData.wavelength = Sp(:,1);
+calibrationData.ujoulepercount = lampIrradiance*(T * A * dLp)./(Sp(:,2) - Dp(:,2));
+calibrationData.integrationtime = T;
+calibrationData.A = A;
+
+[saveFilename saveFileDirectory] = uiputfile('', 'Select filename for saving of calibration file', '.mat');
+save(strcat(saveFileDirectory, saveFilename), 'calibrationData');
+
+end
+
